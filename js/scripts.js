6e1466605e0be2761e8434e7edc2135d313530ad
r-craig73 <ron.craig@comcast.net>
2018-01-19 11:15:30 -0800

    add for loops for numbers 0, 1, and 3

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,25 @@ var newBeepBoop = function(input){
   alert("loop # " + i);
   results[i] = i;
   }
-  alert(results);
+  alert("results array: " + results);
+  for (var i = 0; i < results.length; i++) {
+    if (results[i] === 0) {
+      results[i] = "Beep!";
+    }
+  }
+  alert("second results array:" + results);
+  for (var i = 0; i < results.length; i++) {
+    if (results[i] === 1) {
+      results[i] = "Boop!";
+    }
+  }
+  alert("third results array:" + results);
+  for (var i = 0; i < results.length; i++) {
+    if (results[i] === 3) {
+      results[i] = "I'm sorry Dave. I'm afraid I can't do that.";
+    }
+  }
+  alert("fourth results array:" + results);
 }
 
 
@@ -19,8 +37,6 @@ $("form#beepBoopForm").submit(function(event) {
   var results = newBeepBoop(number);
   $("#results").text(results);
 }); // END event
-
-
 
 
 }); // END ready
