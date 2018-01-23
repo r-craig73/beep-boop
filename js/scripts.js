f6bd4aca33a14e626b197ed55d6053dd35f36846
r-craig73 <ron.craig@comcast.net>
2018-01-23 01:07:33 -0800

    update several documents and limit beep boop search to 100

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,67 @@
-$(document).ready(function () {
-
-// initialize and add filter variables
-var results = [];
+// initialize empty arrays
 var byThreeResults = [];
 var lists = [];
 
-// variables I will use for the next submit
-var boopOneResults = [];
-var beepZeroResults = [];
-// possible string regular expressions filters for beep and boop
-var boop1 = /[1]/;
-var beep0 = /[0]/;
+// Boop! and Beep! Arrays
+var boopOneResults = [0, "Boop!", 2, 3, 4, 5, 6, 7, 8, 9, "Boop!", "Boop!",
+"Boop!", "Boop!", "Boop!", "Boop!", "Boop!", "Boop!", "Boop!", "Boop!", 20,
+"Boop!", 22, 23, 24, 25, 26, 27, 28, 29, 30, "Boop!", 32, 33, 34, 35, 36, 37,
+38, 39, 40, "Boop!", 42, 43, 44, 45, 46, 47, 48, 49, 50, "Boop!", 52, 53, 54,
+55, 56, 57, 58, 59, 60, "Boop!", 62, 63, 64, 65, 66, 67, 68, 69, 70, "Boop!",
+72, 73, 74, 75, 76, 77, 78, 79, 80, "Boop!", 82, 83, 84, 85, 86, 87, 88, 89,
+90, "Boop!", 92, 93, 94, 95, 96, 97, 98, 99, 100];
+var beepZeroResults = ["Beep!", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+12, 13, 14, 15, 16, 17, 18, 19, "Beep!", 21, 22, 23, 24, 25, 26, 27, 28, 29,
+"Beep!", 31, 32, 33, 34, 35, 36, 37, 38, 39, "Beep!", 41, 42, 43, 44, 45, 46,
+47, 48, 49, "Beep!", 51, 52, 53, 54, 55, 56, 57, 58, 59, "Beep!", 61, 62, 63, 64,
+65, 66, 67, 68, 69, "Beep!", 71, 72, 73, 74, 75, 76, 77, 78, 79, "Beep!", 81, 82,
+83, 84, 85, 86, 87, 88, 89, "Beep!", 91, 92, 93, 94, 95, 96, 97, 98, 99, "Beep!"];
 
-var beepBoopByThree = function(input){
-  for (var i = 0; i <= input; i++) {
-  results[i] = i;  //creates the array of numbers
-  }
-  // alert("results array: " + results);
-  for (var i = 0; i < results.length; i++) {
-    if ((results[i] / 3) === 0) {
-      results[i] = "Beep!";  //replace results[0]=Beep!
-    }
-    else if (results[i] % 3 === 0) {
-      results[i] = "I'm sorry Dave. I'm afraid I can't do that.";
-      //add sorry Dave string when number is divisible by three
-    }
-  }
-  var byThreeResults = results.slice(); //  creates a new variable
-  // console.log(byThreeResults);
-  displayBeepBoops(byThreeResults); // output to HTML file
-}
-
-function displayBeepBoops(lists) {
-  lists.forEach(function(list) {
-  $("#results1").append("<h5>"+ list + "</h5>");
-});
-}
-
-$("form#beepBoopForm").submit(function(event) {
+$(document).ready(function () {
+  $("form#beepBoopForm").submit(function(event) {
   event.preventDefault();
+  var results = [];
   var number = parseInt($("input#beepBoop").val());
-  // alert("Input number is " + number);
-  var results = beepBoopByThree(number);
-  // alert(byThreeResultsStrings);
+
+  for (var i = 0; i <= number; i++) {
+  results[i] = i;  //create array of numbers
+  }
+
+  ByThree = function(input){
+    for (var i = 0; i < results.length; i++) {
+      if ((results[i] / 3) === 0) {
+        results[i] = "Beep!";  // results[0]=Beep!
+      }
+      else if (results[i] % 3 === 0) {
+        results[i] = "I'm sorry Dave. I'm afraid I can't do that.";
+        //add "sorry Dave" string when number is divisible by three
+      }
+      else if (results[i] === 1) {
+        results[i] = "Boop!"; // results[1]=Boop!
+      }
+      else if ((typeof(results[i]) === 'number') && (typeof(boopOneResults[i]) === 'string')) {
+        results[i] = boopOneResults[i];
+      }
+      else if ((typeof(results[i]) === 'number') && (typeof(beepZeroResults[i]) === 'string')) {
+        results[i] = beepZeroResults[i];
+      }
+      else {
+        results[i] = i;
+      }
+    }
+    var byThreeResults = results.slice(); //  clone an array
+    displayBeepBoops(byThreeResults); // output to HTML file
+  }
+
+  ByThree(number);
+
+  function displayBeepBoops(lists) {
+    lists.forEach(function(list) {
+    $("#results1").append("<h5>"+ list + "</h5>");
+  });
+  }
+
   $(".results").fadeIn();
-}); // END event
+  }); // END event
+
 }); // END ready
--- readme.md
+++ readme.md
@@ -8,12 +8,12 @@
 
 ## Brief Description
 ### A program that allows a user to input a number.
-### The program will return a range of numbers from 0 to the chosen number.
+### The program will return a range of numbers from 0 to 100.
 ### In between, there will be three different exceptions.
 
 ## Setup
-### A program that allows a user to input a number.
-### The program will return a range of numbers from 0 to the chosen number.
+### A website that allows a user to input a positive, integer number and returns a range of numbers
+### (starting from 0) to the selected number.
 ### In between, there will be three different exceptions.
 ### (1) Numbers divisible by 3 are replaced with "I'm sorry Dave. I'm afraid I can't do that."
 ### (2) Numbers containing 1 are replaced (all digits) with "Boop!"
@@ -29,29 +29,29 @@
 #### Input: 0
 #### Output: [Beep!]
 
-### Program replaces 3 with numbers (start:0, end:3) and exceptions. (NA)
+### Program replaces 3 with numbers (start:0, end:3) and exceptions. (Done)
 #### Input: 3
 #### Output: [Beep!] [Boop!] [2] [I'm sorry Dave. I'm afraid I can't do that.]
 
-### Program replaces 4 with numbers (start:0, end:4) and exceptions. (NA)
+### Program replaces 4 with numbers (start:0, end:4) and exceptions. (Done)
 #### Input: 4
 #### Output: [Beep!] [Boop!] [2] [I'm sorry Dave. I'm afraid I can't do that.] [4]
 
-### Program replaces 5 with numbers (start:0, end:5) and exceptions. (NA)
+### Program replaces 5 with numbers (start:0, end:5) and exceptions. (Done)
 #### Input: 5
 #### Output: [Beep!] [Boop!] [2] [I'm sorry Dave. I'm afraid I can't do that.] [4] [5]
 
-### Program replaces 6 with numbers (star: 0, end:6) and exceptions. (NA)
+### Program replaces 6 with numbers (star: 0, end:6) and exceptions. (Done)
 #### Input: 6
 #### Output: [Beep!] [Boop!] [2] [I'm sorry Dave. I'm afraid I can't do that.] [4] [5] [I'm sorry Dave. I'm afraid I can't do that.]
 
-### Program replaces numbers containing 1 with "Boop!" and exceptions. (NA)
-#### Input: 10, 100, 1000
-#### Output: [Beep!] [Boop!] [2].....[Boop!]
-
-### Program replaces numbers containing 0 with "Beep!" (NA)
+### Program replaces numbers containing 0 with "Beep!" (Done)
 #### Input: 20, 30, 40, 50
 #### Output: [Beep!] [Boop!]....[Beep!]
+
+### Program replaces numbers containing 1 with "Boop!" and exceptions. (NA)
+#### Input: 10, 100
+#### Output: [Beep!] [Boop!] [2].....[Boop!]
 
 ### Program replaces 1 with values from 0 and ends at 1. Exceptions are included for values less than 1. (NA)
 #### Input: 1
